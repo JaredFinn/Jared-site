0036365b3497507e29c10e40dc86111fc6afed10
Jared Finn <jaredhfinn@gmail.com>
2023-03-23 22:51:46 -0400

    gradient change + remove header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'finn-tech';
+
+  name!: string;
+  email!: string;
+  phone!: string;
+  proposal!: string;
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    console.log("Sending Email...")
+    // const formData = {
+    //   name: this.name,
+    //   email: this.email,
+    //   phone: this.phone,
+    //   proposal: this.proposal
+    // };
+
+    // this.http.post('http://localhost:3000/send-email', formData)
+    //   .subscribe(() => {
+    //     console.log('Email sent successfully');
+    //   }, error => {
+    //     console.log('Error sending email:', error);
+    //   });
+  }
 }
