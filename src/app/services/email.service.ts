4d291e4e5b591f61a87ebb3682b6242804372280
Jared Finn <jaredhfinn@gmail.com>
2023-03-29 22:41:48 -0400

    added services

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -10,13 +10,13 @@ export class EmailService {
   sendEmail(subject: string, message: any) {
     const httpOptions = {
       headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Origin': 'http://ec2-3-216-125-179.compute-1.amazonaws.com',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
       })
     };
     const payload = { subject: subject, message: message };
     console.log(payload)
-    return this.http.post('http://localhost:3000/send-email', payload, httpOptions);
+    return this.http.post('http://3.216.125.179:3000/send-email', payload, httpOptions);
   }
 }
