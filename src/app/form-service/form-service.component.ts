0fa8b0b4971e3670d68f291196c1b72681234e3c
Jared Finn <jaredhfinn@gmail.com>
2023-04-19 21:56:15 -0400

    very last

--- src/app/form-service/form-service.component.ts
+++ src/app/form-service/form-service.component.ts
@@ -35,7 +35,7 @@ export class FormServiceComponent implements OnInit {
       fromObject: formData
     });
 
-    this.http.post('/', params.toString(), {
+    this.http.post('/contact', params.toString(), {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
