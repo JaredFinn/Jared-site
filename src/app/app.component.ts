36a52d60b11730b96635b60e5aebd4391fc3f3ec
Jared Finn <jaredhfinn@gmail.com>
2023-05-15 20:06:56 -0400

    new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { trigger, state, transition, animate, style } from '@angular/animations';
+import 'animate.css';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+  @ViewChild('dividerHeading', {static: true}) dividerHeading!: ElementRef;
 
   name: string = '';
   email: string = '';
@@ -15,12 +20,58 @@ export class AppComponent {
 
   submitted: boolean = false;
 
-  constructor() {}
+  view = { type: '' };
+  word = { skill: ['Websites', 'Apps', 'Data Management', 'Automation', ''] };
+  
+  idx = 0;
+  n = 0;
+  up = true;
 
-  @ViewChild('introSection', {static: true}) introSection!: ElementRef;
-  @ViewChild('serviceTitle', {static: true}) serviceTitle!: ElementRef;
-  @ViewChild('serviceRows', {static: true}) serviceRows!: ElementRef;
-  @ViewChild('contact', {static: true}) contact!: ElementRef;
+  boxes = [
+    {
+      imageUrl: '../assets/Jared_Cows.jpg',
+      buttonText: 'Button 1'
+    },
+    {
+      imageUrl: 'path/to/image2.jpg',
+      buttonText: 'Button 2'
+    },
+    {
+      imageUrl: 'path/to/image3.jpg',
+      buttonText: 'Button 3'
+    },
+    // add more boxes here
+  ];
+
+  constructor() {
+    setInterval(() => {
+      const word = this.word.skill[this.idx];
+      const ln = word.length;
+
+      if (this.up) {
+        this.view.type = word.slice(0, this.n);
+        this.n++;
+      }
+      if (this.n === ln + 1) {
+        this.up = false;
+      }
+      if (!this.up) {
+        this.view.type = word.slice(0, this.n);
+        this.n--;
+      }
+      if (this.n === 0) {
+        this.up = true;
+        this.idx++;
+      }
+      if (this.idx === this.word.skill.length) {
+        this.idx = 0;
+      }
+    }, 200);
+  }
+
+
+  drop: boolean = false;
+
 
   ngOnInit() {
     const options = {
@@ -28,64 +79,21 @@ export class AppComponent {
       rootMargin: '10px',
       threshold: .1
     };
-    window.scrollTo(0, 0);
 
     const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.introSection.nativeElement.classList.add('show');
-          observer.unobserve(entry.target);
-        }
-      });
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        this.dividerHeading.nativeElement.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
     }, options);
 
-    const observer2 = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.serviceRows.nativeElement.classList.add('show');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, options);
+    observer.observe(this.dividerHeading.nativeElement);
+    setTimeout(() => {
+      this.drop = true;
+    }, 4000);
 
-    const observer3 = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.serviceTitle.nativeElement.classList.add('show');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, options);
-
-    const observer4 = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.contact.nativeElement.classList.add('show');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, options);
-
-    observer.observe(this.introSection.nativeElement);
-    observer2.observe(this.serviceRows.nativeElement);
-    observer3.observe(this.serviceTitle.nativeElement);
-    observer4.observe(this.contact.nativeElement);
-  }
-
-  sendEmail() {
-    // const subject = 'Inquiry';
-    // const message = {
-    //   name: this.name,
-    //   email: this.email,
-    //   phone: this.phone,
-    //   proposal: this.proposal
-    // };
-    this.submitted = true;
-    // console.log(message)
-    // this.emailService.sendEmail(subject, message).subscribe(response => {
-
-    //   console.log(response);
-    // });
   }
 
   scroll(el: HTMLElement) {
